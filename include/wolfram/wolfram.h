212d0912d575bddeff6d6922a41252d92d7a9ce9
Denis Dedkov <dephyys@gmail.com>
2021-11-23 17:54:42 +0300

    Add wolfram node types file

---
+++ include/wolfram/wolfram.h
@@ -0,0 +1,12 @@
+#ifndef WOLFRAM_H
+#define WOLFRAM_H
+
+#include <stdlib.h>
+
+enum wf_type {
+        WF_VARIABLE = 0x01,
+        WF_OPERATOR = 0x02,
+        WF_LITERAL  = 0x03,
+};
+
+#endif /* WOLFRAM_H */
